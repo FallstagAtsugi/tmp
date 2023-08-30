7cd46097996110883e249d33fe6edc913fb69d0a
kboss <atsugi.design@gmail.com>
2023-08-31 07:48:04 +0900

    演算子

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ let tomatoCount; //キャメルケース JSのベストプラクティス
 let tomato_count; //スネークケース
 let $tomatoCount;
 let _tomatoCount;
+
+let add = 2 + 5;
+console.log(add);
+
+//余り
+console.log(32 % 5);
+
+//累乗
+console.log(2 ** 3);
