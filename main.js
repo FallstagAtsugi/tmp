0756c0d64891519fb189a0e3699e0d86bb27b994
kboss <atsugi.design@gmail.com>
2023-09-04 15:50:31 +0900

    アロー関数

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const name = "柿崎";
 const age = 26;
 console.log(`私の名前は${name}です。年齢は${age}です。`);
+
+// function add(a, b) {
+//     return a - b;
+// }
+
+//アロー関数
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(1, 4));
