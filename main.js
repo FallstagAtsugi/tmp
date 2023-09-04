9774a40195a7e527debcce6d72ce5db195b468ea
kboss <atsugi.design@gmail.com>
2023-09-04 16:09:58 +0900

    スプレッド構文

--- main.js
+++ main.js
@@ -7,11 +7,11 @@
 // }
 
 //アロー関数
-const add = (a, b) => {
-  return a + b;
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
 
-console.log(add(1, 4));
+// console.log(add(1, 4));
 
 //分割代入
 // const myProfile = {
@@ -26,11 +26,41 @@ console.log(add(1, 4));
 // const message2 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
-const myProfile = ["柿崎", 26];
+// const myProfile = ["柿崎", 26];
 
-const messege3 = `名前は${myProfile[0]},年齢は${myProfile[1]}`;
-console.log(messege3);
+// const messege3 = `名前は${myProfile[0]},年齢は${myProfile[1]}`;
+// console.log(messege3);
 
-const [name, age] = myProfile;
-const messege4 = `${name},${age}`;
-console.log(messege4);
+// const [name, age] = myProfile;
+// const messege4 = `${name},${age}`;
+// console.log(messege4);
+
+// //スプレッド構文
+// //配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (a, b) => {
+//   console.log(a + b);
+// };
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1); //配列を順番に処理する
+
+//まるめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(...arr3);
+
+//配列のコピー、結合 よくReactでも使う
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
