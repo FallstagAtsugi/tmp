8cb616cecde801e1eaaf5be17dd12255f3cf91d0
kboss <atsugi.design@gmail.com>
2023-09-05 22:30:17 +0900

    テキストボックスの値を取得する

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import "./styles.css";
+// import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inuptText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "a";
+  alert(inuptText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
