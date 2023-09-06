e0a95f1a6a5b863ca171527f0d6d03eb3fda8f55
kboss <atsugi.design@gmail.com>
2023-09-06 17:09:33 +0900

    入力した値を出力する

--- index.js
+++ index.js
@@ -3,8 +3,22 @@
 const onClickAdd = () => {
   //テキストボックスの値を取得し、初期化する
   const inuptText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "a";
-  alert(inuptText);
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inuptText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
