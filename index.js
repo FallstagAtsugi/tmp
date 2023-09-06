42bd248722a1080417fad5ee78998d9ada4411a5
kboss <atsugi.design@gmail.com>
2023-09-06 17:17:18 +0900

    divタグの子要素に各要素を設定

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inuptText;
 
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   //divタグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
